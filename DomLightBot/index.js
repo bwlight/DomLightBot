2f6436f4b65c9525c7928f1f4b2ffae5edd11ce8
bwlight <bwlight1@gmail.com>
2019-08-19 07:18:55 -0500

    test 2

--- DomLightBot/index.js
+++ DomLightBot/index.js
@@ -5,9 +5,18 @@ bot.on("ready", () => {
   console.log("I am ready!");
 });
 
-bot.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
+
+// Set the prefix
+let prefix = "~";
+client.on("message", (message) => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
   }
 });
 
