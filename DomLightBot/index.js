3813fbb56c2d14c6dd952ac4ef7a67f8c977ba83
bwlight <bwlight1@gmail.com>
2019-08-19 05:41:36 -0500

    tester2

--- DomLightBot/index.js
+++ DomLightBot/index.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
  
-Client.on("ready", () => {
+bot.on("ready", () => {
   console.log("I am ready!");
 });
  
-Client.on("message", (message) => {
+bot.on("message", (message) => {
   if (message.content.startsWith("ping")) {
     message.channel.send("pong!");
   }
 });
  
-Client.login('TCiZk-KMM15yPQDQVTZgLcuuoyt9A4rc');
+bot.login('TCiZk-KMM15yPQDQVTZgLcuuoyt9A4rc');
